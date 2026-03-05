0fbbb7b2894e4c0b6fd93fe4b060e53a722a660c
njooris <njooris@njooris.42.fr>
2026-03-05 08:15:47 -0600

    on sait jamais

--- mariadb/entrypoint.sh
+++ mariadb/entrypoint.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-	mariadb-install-db --user=mysql --datadir=/var/lib/mysql
-	db_pwd="test123"
-	mariadbd --datadir=/var/lib/mysql &
-	until mariadb-admin ping -h 127.0.0.1 -u root &> /dev/null; do
-		sleep 0.1
-	done
+DB_PWD="test123"
 
-	 mariadb -h 127.0.0.1 -u root -e "CREATE DATABASE myDataBase"
-	 mariadb -h 127.0.0.1 -u root -e "CREATE USER 'njooris'@'%' IDENTIFIED BY '$db_pwd'"
-	 mariadb -h 127.0.0.1 -u root -e "GRANT ALL PRIVILEGES ON myDataBase.* TO 'njooris'@'%';"
-	 mariadb -h 127.0.0.1 -u root -e "FLUSH PRIVILEGES;"
-	 mariadb-admin -u root shutdown
+if [ ! -d "$DATA_DIR/mysql" ]; then
+    mariadb-install-db --user=mysql --datadir="$DATA_DIR"
 
+    mariadbd --datadir="$DATA_DIR" &
+
+    until mariadb-admin ping -h 127.0.0.1 --silent; do
+        sleep 0.1
+    done
+
+    mariadb -h 127.0.0.1 -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_PWD';"
+    mariadb -h 127.0.0.1 -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+    mariadb -h 127.0.0.1 -u root -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD';"
+    mariadb -h 127.0.0.1 -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';"
+    mariadb -h 127.0.0.1 -u root -e "FLUSH PRIVILEGES;"
+
+    pkill -f mariadbd
 fi
-exec mariadbd --user=mysql --datadir=/var/lib/mysql
+
+exec mariadbd --user=mysql --datadir="$DATA_DIR"
+
