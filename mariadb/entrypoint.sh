4d00e528e56816adf7e0843ccc14b0ad59ae437b
njooris <njooris@njooris.42.fr>
2026-03-05 04:41:00 -0600

    mariadb

--- mariadb/entrypoint.sh
+++ mariadb/entrypoint.sh
@@ -1,6 +1,18 @@
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -keyout /etc/nginx/ssl/key.pem \
-    -out /etc/nginx/ssl/cert.pem \
-    -subj "/CN=localhost"
+#!/bin/sh
 
-exec nginx -g "daemon off;"
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+	mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+	db_pwd="test123"
+	mariadbd --datadir=/var/lib/mysql &
+	until mariadb-admin ping -h 127.0.0.1 -u root &> /dev/null; do
+		sleep 0.1
+	done
+
+	 mariadb -h 127.0.0.1 -u root -e "CREATE DATABASE myDataBase"
+	 mariadb -h 127.0.0.1 -u root -e "CREATE USER 'njooris'@'%' IDENTIFIED BY '$db_pwd'"
+	 mariadb -h 127.0.0.1 -u root -e "GRANT ALL PRIVILEGES ON myDataBase.* TO 'njooris'@'%';"
+	 mariadb -h 127.0.0.1 -u root -e "FLUSH PRIVILEGES;"
+	 mariadb-admin -u root shutdown
+
+fi
+exec mariadbd --user=mysql --datadir=/var/lib/mysql
