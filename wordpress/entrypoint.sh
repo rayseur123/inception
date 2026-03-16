02e90c1c8784519ae3506c385d18a3722eb7dc0f
rayseur123 <Nicolas.jooris@hotmail.fr>
2026-03-16 11:21:11 -0500

    patch

--- wordpress/entrypoint.sh
+++ wordpress/entrypoint.sh
@@ -3,9 +3,14 @@
 cd /var/www/html
 
 DB_PWD=$(cat /run/secrets/db_password)
+ADMIN_USR=$(cat /run/secrets/wp_admin_user)
+ADMIN_PWD=$(cat /run/secrets/wp_admin_pwd)
+ADMIN_EMAIL=$(cat /run/secrets/wp_admin_mail)
+USER_USR=$(cat /run/secrets/wp_user_user)
+USER_PWD=$(cat /run/secrets/wp_user_pwd)
+USER_EMAIL=$(cat /run/secrets/wp_user_mail)
 
 if [ ! -f "wp-config.php" ]; then
-    echo "test11111111111"
     cp wp-config-sample.php wp-config.php
 
     sed -i "s|database_name_here|$DB_NAME|g" wp-config.php
@@ -15,22 +20,19 @@ if [ ! -f "wp-config.php" ]; then
 
 fi
 
-sleep 10
-
 if ! wp core is-installed; then
-	echo "test222222222"
 	wp core install \
 	--url=njooris.42.fr \
 	--title="Inception" \
-	--admin_user=$WP_ADMIN_USR \
-	--admin_password=$WP_ADMIN_PWD \
-	--admin_email=$WP_ADMIN_EMAIL \
+	--admin_user=$ADMIN_USR \
+	--admin_password=$ADMIN_PWD \
+	--admin_email=$ADMIN_EMAIL \
 	--skip-email \
 	--allow-root
 	
-	wp user create $WP_USER $WP_USER_EMAIL \
+	wp user create $USER_USR $USER_EMAIL \
 	--role=author \
-	--user_pass=$WP_USER_PWD \
+	--user_pass=$USER_PWD \
 	--allow-root
 fi
 
